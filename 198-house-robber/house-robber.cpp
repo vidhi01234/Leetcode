998b2f0d1316436ef6549f1ee34e3e688083ee59
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-07-22 00:39:11 +0530

    Add memoization and begin tabulation for robbing houses
    Implemented memoization for the house robber problem and started tabulation with space optimization.

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,3 +1,28 @@
+//Memoization
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return f(n-1,nums,dp);
+    }
+
+    int f(int idx,vector<int>& nums,vector<int>& dp)
+    {
+        if(idx==0)return nums[idx];
+        if(idx==-1)return 0;
+
+        if(dp[idx]!= -1) return dp[idx];
+
+        int pick= f(idx-2,nums,dp)+nums[idx];
+        int notPick=f(idx-1,nums,dp)+0;
+
+        return dp[idx]=max(pick,notPick); 
+    }
+};
+
+//Tabulation with space optimization
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
