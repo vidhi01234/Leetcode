38ee4978950364af3d5723fdfdf78507ec59c277
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-02 00:28:04 +0530

    Time: 1158 ms (4.50%) | Memory: 71.8 MB (52.71%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            cout<<sum<<endl;
+            sum=max(sum,nums[i]);
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
