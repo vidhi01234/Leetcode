a30ab241a608f8723ed10d9791e295e67a127343
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-03 03:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (48.84%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maximumSum(vector<int>& arr) {
         int noDel=arr[0];
-        int oneDel=INT_MIN;
+        int oneDel=arr[0];
         int res=arr[0];
         for(int i=1;i<arr.size();i++)
         {
             int prevND=noDel;
             noDel=max(arr[i],noDel+arr[i]);
-            if(oneDel==INT_MIN)oneDel=arr[0];
             oneDel=max(oneDel+arr[i],prevND);
             res=max(res,max(noDel,oneDel));
         }
