e61e991acdb69f4d91219ee8353f4c53a87f1932
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-04 16:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (3.04%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        if(n==0)return 0;
+        while(n>0)
+        {
+            if(n%2==1)count++;
+            n=n/2;
+        }
+        return count;
+        
+    }
+};
