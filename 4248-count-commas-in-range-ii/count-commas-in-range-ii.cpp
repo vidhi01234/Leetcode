7725b949895159933aa19b08607fe2438050c852
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-09 21:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     long long countCommas(long long n){ 
-        long long res=0;
-        if(n>=1000)
-            res+=n-1000+1;
-        if(n>=1000000)
-            res+=n-1000000+1;
-        if(n>=1000000000)
-            res+=n-1000000000+1;
-        if(n>=1000000000000)
-            res+=n-1000000000000+1;
-        if(n>=1000000000000000)
-            res+=n-1000000000000000+1;
+        long curr=1000,res=0;
+        while(curr<=n)
+        {
+            res+=n-curr+1;
+            curr*=1000;
+        }
         return res;
     }
 };
