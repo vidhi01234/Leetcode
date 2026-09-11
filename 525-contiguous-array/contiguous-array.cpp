589e64be20952bd026a83470d0c5b99662388834
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-12 02:37:06 +0530

    Time: 27 ms (38.65%) | Memory: 88.3 MB (43.42%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -3,6 +3,7 @@ public:
     int findMaxLength(vector<int>& nums) {
         int one=0,res=0,zero=0,diff=0,idx=0,len=0;
         unordered_map<int,int>mp;
+        mp[0] = -1;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==0)zero++;
