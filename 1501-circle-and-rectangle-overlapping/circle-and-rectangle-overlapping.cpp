efccac4e8515d46d1ed72d4abe47f800f648e772
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-20 01:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (34.96%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkOverlap(int r, int cx, int cy, int x1, int y1, int x2, int y2) {
+        int x= max(x1, min(cx,x2))-cx;
+        int y= max(y1, min(cy,y2))-cy;
+        return x*x+y*y<=r*r;
+    }
+};
