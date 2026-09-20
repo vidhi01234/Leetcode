2e3d8f5ef17c7f6f2964d012e962c2aceb0c55d3
VIDHI SRIVASTAVA <160155332+vidhi01234@users.noreply.github.com>
2026-09-21 02:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.66%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int val=26-(s[i]-'a');
+            res+=val*(i+1);
+        }
+        return res;
+    }
+};
